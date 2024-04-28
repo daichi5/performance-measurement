98d239c01c66da1783a25a4c1a89a7d3a3743e6d
daichi5 <sawa22daichi@gmail.com>
2024-04-29 01:13:35 +0900

    add requestTimeout

--- server/fastify/src/index.ts
+++ server/fastify/src/index.ts
@@ -1,12 +1,16 @@
 import fastify from 'fastify'
 
-const server = fastify()
+const server = fastify({
+  ignoreTrailingSlash: true,
+  requestTimeout: 30000,
+})
 
 server.get('/api/ping', async (request, reply) => {
+  console.log('ping')
   return 'pong\n'
 })
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 8080, host: 'localhost' }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
